81aab454173d1b967f344991eb2ce2500b3ec989
limdumb <dlaruddls@nate.com>
2023-09-20 22:52:28 +0900

    [Feat] Server 추가

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const BASE_URL = "https://chartin.me";
 const accessToken =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMSIsInJvbGUiOiJST0xFX1VTRVIiLCJpc3MiOiJjaGFydGluLm1lIiwiaWF0IjoxNjk0MDcwMzA2LCJleHAiOjE2OTQwODQ3MDZ9.lA0NB_LZ3J5FuXB1f68UeMx_8Ryi6-JyGAXc5luG2jo";
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMSIsInJvbGUiOiJST0xFX1VTRVIiLCJpc3MiOiJjaGFydGluLm1lIiwiaWF0IjoxNjk1MDkwNTQ3LCJleHAiOjE2OTUxMDQ5NDd9.G3q2ujIK6RDvcIBLLeN_iRSgbry4y77Vd2J4D1gvLo8";
 export const baseInstance = axios.create({
   baseURL: BASE_URL,
   headers: { Authorization: accessToken },
